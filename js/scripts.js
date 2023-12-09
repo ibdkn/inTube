0a8ffc6aec41602fc190e9c3b2e8098a774fae6d
ibdkn <ibudiakina@gmail.com>
2023-12-09 13:56:05 +0300

    added object of videos

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+// Data
+videos = [
+    {
+        id: 1,
+        category: 'Education Videos',
+        vides: [
+            {
+                videoId: 1,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: true,
+            },
+            {
+                videoId: 2,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: false,
+            },
+            {
+                videoId: 2,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: false,
+            }
+        ]
+    },
+    {
+        id: 2,
+        category: 'Education Videos',
+        vides: [
+            {
+                videoId: 1,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: true,
+            },
+            {
+                videoId: 2,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: false,
+            },
+            {
+                videoId: 2,
+                title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
+                fileUrl: 'https://www.youtube.com/embed/bmHZ-YQBWNA?si=WD2hFStYPG_b0MZH',
+                videoChannel: 'It-Kamasutra',
+                publicationDate: '1 week ago',
+                live: false,
+            }
+        ]
+    },
+]
+
+// render
+renderVideolist();
