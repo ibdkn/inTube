e379d6f1fb566e13e03d36a6001247de7303054f
ibdkn <ibudiakina@gmail.com>
2023-12-12 19:17:08 +0300

    added js functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // Data
-videos = [
+data = [
     {
         id: 1,
         category: 'Education Videos',
-        vides: [
+        videos: [
             {
                 videoId: 1,
                 title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
@@ -33,7 +33,7 @@ videos = [
     {
         id: 2,
         category: 'Education Videos',
-        vides: [
+        videos: [
             {
                 videoId: 1,
                 title: 'JavaScript - Way of the Samurai, Browser, first program, foreign language learning simulator',
@@ -62,5 +62,85 @@ videos = [
     },
 ]
 
+// variables
+let videosMainElement = document.getElementById('playlists');
+
 // render
-renderVideolist();
+renderVideolist(data);
+
+function renderVideolist(playlistForRendering) {
+
+    playlistForRendering.forEach(video => {
+        const { id, category, videos: [ ...videos ] } = video;
+
+        let playlistElement = document.createElement('div');
+        playlistElement.classList.add('playlist');
+
+        let playlistTitleElement = document.createElement('h2');
+        playlistTitleElement.classList.add('playlist__title');
+        playlistTitleElement.innerText = category;
+
+        playlistElement.append(playlistTitleElement);
+
+        videosMainElement.append(playlistElement);
+
+        renderVideo(videos, playlistElement);
+    })
+
+}
+
+function renderVideo(videos, element) {
+    let videosWrapperElement = document.createElement('ul');
+    videosWrapperElement.classList.add('videos');
+
+    if(videos.length > 0) {
+        videos.forEach(video => {
+            let videoWrapperElement = document.createElement('li');
+            videoWrapperElement.classList.add('video');
+
+            let videoElement = document.createElement('iframe');
+            videoElement.classList.add('video__frame');
+            videoElement.setAttribute('src', video.fileUrl);
+            videoElement.setAttribute('title', 'YouTube video player');
+            videoElement.setAttribute('frameborder', '0');
+            videoElement.setAttribute('allow', 'accelerometer');
+            videoElement.setAttribute('autoplay', '');
+            videoElement.setAttribute('clipboard-write', '');
+            videoElement.setAttribute('encrypted-media', '');
+            videoElement.setAttribute('allowfullscreen', '');
+            videoElement.setAttribute('gyroscope', '');
+            videoElement.setAttribute('picture-in-picture', '');
+            videoElement.setAttribute('web-share', '');
+
+            let videoTitleElement = document.createElement('h2');
+            videoTitleElement.classList.add('video__title');
+            videoTitleElement.innerText = video.title;
+
+            let videoChannelElement = document.createElement('p');
+            videoChannelElement.classList.add('video__channel');
+            videoChannelElement.innerText = video.videoChannel;
+
+
+            let videoDateElement = document.createElement('p');
+            videoDateElement.classList.add('video__date');
+            videoDateElement.innerText = video.publicationDate;
+
+            if(video.live) {
+                let videoLiveElement = document.createElement('span');
+                videoLiveElement.classList.add('live');
+                videoLiveElement.innerText = 'Live Stream';
+                videoDateElement.innerText = '';
+                videoDateElement.append(videoLiveElement);
+            }
+
+            videoWrapperElement.append(videoElement);
+            videoWrapperElement.append(videoTitleElement);
+            videoWrapperElement.append(videoChannelElement);
+            videoWrapperElement.append(videoDateElement);
+
+            videosWrapperElement.append(videoWrapperElement);
+            element.append(videosWrapperElement);
+        });
+
+    }
+}
